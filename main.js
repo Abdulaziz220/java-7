3a4107953182e12aef01aebde0e9df85c2bc1302
Your Name <you@example.com>
2024-05-02 13:06:14 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let a = ["Olma", "Banan", "Anor", "Shoftoli"];
+let b = a.length;
+alert("Arriynig uzunligi" + b);
+let t ;
+console.log(a)
+let d =  confirm("Sizning array malumotlarni olib tashlamoqchimisiz");
+if (d) {
+    t = a.pop()
+}
+
+console.log();
+console.log(a);
